7a08f6a8c0066423515286bc40ae4cf83f98104d
II <picus.2022@mail.ru>
2026-07-31 11:07:53 +0300

    add .env.local

--- environment/env/env.go
+++ environment/env/env.go
@@ -2,6 +2,7 @@ package env
 
 import (
 	"log"
+	"os"
 	"strings"
 
 	"github.com/spf13/viper"
@@ -12,11 +13,22 @@ type EnvConfig = *viper.Viper
 var Config = viper.New()
 
 func init() {
+	// 1. Загрузка основного файла .env
 	Config.SetConfigFile(".env")
 	Config.SetConfigType("env")
 
 	if err := Config.MergeInConfig(); err != nil {
 		log.Fatal("Не удалось прочитать .env")
+	}
+
+	// 2. Наложение .env.local, если он существует
+	if _, err := os.Stat(".env.local"); err == nil {
+		Config.SetConfigFile(".env.local")
+		Config.SetConfigType("env")
+
+		if err := Config.MergeInConfig(); err != nil {
+			log.Printf("Предупреждение: .env.local найден, но не прочитан: %v", err)
+		}
 	}
 
 	replaceDots()
